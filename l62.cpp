b65dc18d2fb7850d9a3c9164553efd6f973ec981
Phan Thi Hai Ha <phantransmith@gmail.com>
2019-05-18 02:40:23 +0300

    update

--- l62.cpp
+++ l62.cpp
@@ -11,39 +11,48 @@ using namespace std;
 
 sem_t *sem;
 FILE *fn;
-typedef struct someArgs_tag {
+typedef struct Args_tag 
+{
     bool flag1;
-} someArgs_t;
+} Args_t;
 
-void* func1(void *args) {
-    someArgs_t *arg = (someArgs_t*) args;
-while(arg->flag1){
+void* func1(void *args) 
+{
+    Args_t *arg = (Args_t*) args;
+    while(arg->flag1)
+    {
         sleep(1);
         char a = '2';
         sem_wait(sem);
         for (int i=1;i<=5;i++)
         {
-        cout<<"2"<<flush;
-        fputs("2",fn);
-        sleep(1);
+            cout<<"2"<<flush;
+            fputs("2",fn);
+            sleep(1);
         }
         sem_post(sem);
   }
 }
 
-int main() {
-    pthread_t id1;
-    fn = fopen("file1.txt", "a");
-    sem = sem_open("/mysem",O_CREAT,0644, 1);
-    someArgs_t args;
-  args.flag1=true;
-
+int main()
+{
+   pthread_t id1;
+   fn = fopen("abc.txt", "a");
+   sem = sem_open("/mysem",O_CREAT,0644, 1);
+    
+   Args_t args;
+   args.flag1=true;
+    
    pthread_create(&id1, NULL, func1,(void*)&args);
-getchar();
-  args.flag1 = 0;
-  pthread_join(id1, NULL);
-  sem_close(sem);
-  sem_unlink("/mysem");
-  fclose(fn);
-    return 0;
+   getchar();
+    
+   args.flag1 = 0;
+    
+   pthread_join(id1, NULL);
+    
+   sem_close(sem);
+   sem_unlink("/mysem");
+    
+   fclose(fn);
+   return 0;
 }
